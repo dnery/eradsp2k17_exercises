510a1a873805964a86b9eaec6494d2af1f685819
Danilo <dznery@gmail.com>
2017-04-20 21:32:11 -0300

    Implement mult() routine

--- problem-A.cc
+++ problem-A.cc
@@ -27,10 +27,11 @@ struct bignum {
 
 
 
-void bn_sum(bignum &dest, const bignum &a, const bignum &b);
-void bn_pad(bignum &a, bignum &b);
-void bn_trim(bignum &number);
-void bn_print(bignum &number);
+void bn_add(bignum &dest, const bignum &, bignum &);
+void bn_sub(bignum &dest, const bignum &, bignum &);
+void bn_pad(bignum &, bignum &);
+void bn_trim(bignum &);
+void bn_print(bignum &);  // PURE
 
 
 
@@ -72,8 +73,8 @@ void bn_pad(bignum &a, bignum &b)
 void bn_trim(bignum &number)
 {
         // to and fro (ignore this)
-        auto increase = [](char a) { return (a != '.' ? a + 48 : a); };
-        auto decrease = [](char a) { return (a != '.' ? a - 48 : a); };
+        auto increase = [](char a) { return a + 48; };
+        auto decrease = [](char a) { return a - 48; };
 
         // convert to string... (ignore this)
         std::transform(number.digits.begin(), number.digits.end(),
@@ -104,10 +105,21 @@ void bn_trim(bignum &number)
  * add bignum b to bignum b and store in bignum dest
  *
  * Bignums a and b _must_ be aligned in length and decimal point (see bn_pad)
- * and the result is not trimmed (see bn_trim)
+ * and the result is not trimmed (see bn_trim).
  */
-void bn_add(bignum &dest, const bignum &a, const bignum &b)
+void bn_add(bignum &dest, const bignum &a, bignum &b)
 {
+        /*
+         * (a) + (-b) = (a - b)
+         * (-a) + (b) = -(a - b)
+         */
+        if (a.signal != b.signal) {
+                // try to alter as little state as possible
+                bignum c{!b.signal, b.digits, b.dpoint};
+                bn_sub(dest, a, c);
+                return;
+        }
+
         dest.signal = a.signal;
         dest.digits = a.digits;
         dest.length = a.length + 1;
@@ -127,10 +139,24 @@ void bn_add(bignum &dest, const bignum &a, const bignum &b)
 
 /*
  * subtract bignum b from bignum a and store in bignum dest
+ *
+ * Bignums a and b _must_ be aligned in length and decimal point (see bn_pad)
+ * and the result is not trimmed (see bn_trim).
  */
-void bn_sub(bignum &dest, const bignum &a, const bignum &b)
+void bn_sub(bignum &dest, const bignum &a, bignum &b)
 {
-        dest.signal = a.signal;
+        /*
+         * (a) - (-b) = (a + b)
+         * (-a) - (b) = -(a + b)
+         */
+        if (a.signal != b.signal) {
+                // try to alter as little state as possible
+                bignum c{!b.signal, b.digits, b.dpoint};
+                bn_add(dest, a, c);
+                return;
+        }
+
+        dest.signal = a.signal;  // if a is bigger, the signal maintains
         dest.digits = a.digits;
         dest.length = a.length + 1;
         dest.dpoint = a.dpoint + 1;
@@ -138,7 +164,7 @@ void bn_sub(bignum &dest, const bignum &a, const bignum &b)
         // might be needed for borrowing, if b > a
         dest.digits.insert(0, 1, 0);
 
-        // now add elements in 'b' to current
+        // now sum elements in 'b' from current
         for (size_t i = b.length; i > 0; i--) {
                 dest.digits[i] -= b.digits[i - 1];
 
@@ -167,6 +193,34 @@ void bn_sub(bignum &dest, const bignum &a, const bignum &b)
 }
 
 
+void bn_mult(bignum &dest, bignum &a, bignum &b)
+{
+        /*
+         * length of x ~~ log10(x) + 1 (or, at most one-off from it)
+         *
+         * The length of the resulting multiplication a*b would
+         * log10(a) + log10(b) + 1. Given, the previous, if I already know the
+         * lenghts of a and b (which I do), computing the log10 can be skipped.
+         */
+        dest.signal = (a.signal == b.signal);
+        dest.length = a.length + b.length + 1;
+        dest.digits = std::string(dest.length, '\0');
+        dest.dpoint = dest.length
+                - (a.length - a.dpoint)
+                - (b.length - b.dpoint);
+
+        // now do simple simple multiply
+        #pragma omp parallel for collapse(2)
+        for (size_t i = b.length; i > 0; i--) {
+                for (size_t j = a.length; j > 0; j--) {
+                        dest.digits[i + j] += b.digits[i - 1] * a.digits[j - 1];
+                        dest.digits[i + j - 1] += dest.digits[i + j] / 10;
+                        dest.digits[i + j] = dest.digits[i + j] % 10;
+                }
+        }
+}
+
+
 void bn_print(bignum &number)
 {
         // signal
@@ -181,7 +235,7 @@ void bn_print(bignum &number)
         std::cout << '.';
 
         // decimal part
-        for (size_t i = number.dpoint + 1; i < number.length; i++)
+        for (size_t i = number.dpoint; i < number.length; i++)
                 std::cout << char(number.digits[i] + 48);
 
         std::cout << std::endl;
@@ -190,8 +244,8 @@ void bn_print(bignum &number)
 
 int main(int argc, char *argv[])
 {
-        bignum number1{1, {2, 8, 4, 7, 9, 8, 4, 2, 6, 5, '.', 3, 2, 0, 2}, 10};
-        bignum number2{1, {8, 4, 9, 6, 7, 0, 4, 2, 1, '.', 4, 5, 5, 2, 3}, 9};
+        bignum number1{1, {2, 8, 4, 7, 9, 8, 4, 2, 6, 5, 3, 2, 0, 2}, 10};
+        bignum number2{1, {8, 4, 9, 6, 7, 0, 4, 2, 1, 4, 5, 5, 2, 3}, 9};
 
         std::cout << std::endl;
         std::cout << "================" << std::endl;
@@ -251,6 +305,22 @@ int main(int argc, char *argv[])
                 std::cout << "+ original" << std::endl;
                 bn_print(number2);
                 bn_print(number1);
+
+                std::cout << "+ result" << std::endl;
+                bn_print(result);
+        }
+
+        std::cout << std::endl;
+        std::cout << "==========" << std::endl;
+        std::cout << "Test: Mult" << std::endl;
+        std::cout << "==========" << std::endl;
+        {
+                bignum result;
+                bn_mult(result, number1, number2);
+
+                std::cout << "+ original" << std::endl;
+                bn_print(number1);
+                bn_print(number2);
 
                 std::cout << "+ result" << std::endl;
                 bn_print(result);
